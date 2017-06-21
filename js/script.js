bf85e66afd707b383394dc35e0862b6ea59d9284
apelsin16 <apelsin@i.ua>
2017-06-21 15:07:31 +0300

    start

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-
-
 (function (){
 'use strict';
 
 
 
 
-   var form = document.getElementById('form');
+  var form = document.getElementById('form');
    var buttonStart = document.createElement('input');
    buttonStart.setAttribute('type', 'submit');
    buttonStart.setAttribute('value', 'Start');
    buttonStart.setAttribute('class', 'btn btn-success btn-lg');
    buttonStart.setAttribute('id', 'btn-success');
 
-   var buttonFinish = document.createElement('input');
+  var buttonFinish = document.createElement('input');
    buttonFinish.setAttribute('type', 'submit');
    buttonFinish.setAttribute('value', 'Finish');
    buttonFinish.setAttribute('class', 'btn btn-danger btn-lg');
@@ -35,43 +33,18 @@ var minutes = document.getElementById('minutes');
 var hours = document.getElementById('hours');
 var isActive = false;
 
-var millis;
-var sec;
-var min;
-var hrs;
-function timer () {
-function startTimer () {
-  isActive = true;
-  getTime();
-}
 
 
-function getTime(){
+function getTime (){
    startTime = Date.now();
+   setInterval(function(){
    deltaTime = new Date(Date.now() - startTime);
-   millis = deltaTime.getMilliseconds();
-   sec = deltaTime.getSeconds();
-   min = deltaTime.getMinutes();
-   hrs = deltaTime.getUTCHours();
+
+  seconds.innerHTML = deltaTime.getSeconds();
+   //deltaTime.getMilliseconds();
+
+}, 1000);
 }
 
-function counter() {
-    getTime();
-    ms.innerHTML = millis;
-    seconds.innerHTML = sec;
-    minutes.innerHTML = min;
-    hours.innerHTML = hrs;
-  }
-
-function displayTimer() {
-  timerID = setInterval(counter, 10);
-}
-startBtn[0].addEventListener("click", displayTimer, false);
-}
-timer();
-
-
-
-
-
+startBtn[0].addEventListener("click", getTime, false);
 }());
