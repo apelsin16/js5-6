8d0f0496ce1c78cdb9225d307cddbeaea03fb81d
apelsin16 <apelsin@i.ua>
2017-08-22 13:22:20 +0300

    работает

--- js/script.js
+++ js/script.js
@@ -4,28 +4,14 @@
 
 
 
-  var form = document.getElementById('form');
-   var buttonStart = document.createElement('input');
-   buttonStart.setAttribute('type', 'submit');
-   buttonStart.setAttribute('value', 'Start');
-   buttonStart.setAttribute('class', 'btn btn-success btn-lg');
-   buttonStart.setAttribute('id', 'btn-success');
-
-  var buttonFinish = document.createElement('input');
-   buttonFinish.setAttribute('type', 'submit');
-   buttonFinish.setAttribute('value', 'Finish');
-   buttonFinish.setAttribute('class', 'btn btn-danger btn-lg');
-//
-   form.appendChild(buttonStart);
-   form.appendChild(buttonFinish);
-
 //
 //
 //
 var startTime;
 var deltaTime;
 
-var startBtn = document.getElementById('btn-success');
+var startBtn = document.getElementById('start');
+console.log(startBtn);
 
 var ms = document.getElementById('ms');
 var seconds = document.getElementById('seconds');
@@ -33,28 +19,33 @@ var minutes = document.getElementById('minutes');
 var hours = document.getElementById('hours');
 var isActive = false;
 
+function getTime(e){
+    // e.preventDefault();
+    // console.log('Hello!!!')
 
-
-startBtn.addEventListener('click',function getTime(){
+   // if (isActive !== true){
    startTime = Date.now();
    var interval = setInterval(function(){
    deltaTime = new Date(Date.now() - startTime);
-   html();
- }, 1);
+   createHtml();
 
- });
+    }, 1);
+    e.preventDefault();
+//   } else {
+//     preventDefault();
+//   }
+}
 
-var html = function(){
+var createHtml = function(){
 
  ms.innerHTML = deltaTime.getMilliseconds();
  seconds.innerHTML = deltaTime.getSeconds();
  minutes.innerHTML = deltaTime.getMinutes();
  hours.innerHTML = deltaTime.getUTCHours();
+ isActive = true;
 
 };
-//var html = function () {
-  //getTime();
- //}   ;
 
+startBtn.addEventListener('click', getTime, false);
 
 }());
