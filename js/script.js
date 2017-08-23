d0159eff50895f47b5ff56ebe5725d18e0d83fc8
apelsin16 <apelsin@i.ua>
2017-08-23 18:27:49 +0300

    работает

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 (function (){
-'use strict';
+    'use strict';
 
 
 
@@ -7,45 +7,51 @@
 //
 //
 //
-var startTime;
-var deltaTime;
+    var startTime;
+    var deltaTime;
 
-var startBtn = document.getElementById('start');
-console.log(startBtn);
+    var startBtn = document.getElementById('start');
+    console.log(startBtn);
+    var stopBtn = document.getElementById('stop')
 
-var ms = document.getElementById('ms');
-var seconds = document.getElementById('seconds');
-var minutes = document.getElementById('minutes');
-var hours = document.getElementById('hours');
-var isActive = false;
+    var ms = document.getElementById('ms');
+    var seconds = document.getElementById('seconds');
+    var minutes = document.getElementById('minutes');
+    var hours = document.getElementById('hours');
+    var isActive = false;
 
-function getTime(e){
-    // e.preventDefault();
-    // console.log('Hello!!!')
+    function getTime(e){
+        // e.preventDefault();
+        //
 
-   // if (isActive !== true){
-   startTime = Date.now();
-   var interval = setInterval(function(){
-   deltaTime = new Date(Date.now() - startTime);
-   createHtml();
+        if (isActive !== true){
+        startTime = Date.now();
+        var interval = setInterval(function(){
+            deltaTime = new Date(Date.now() - startTime);
+            createHtml();
+            startBtn.setAttribute('value','Пауза');
+            // e.preventDefault();
 
-    }, 1);
-    e.preventDefault();
-//   } else {
-//     preventDefault();
-//   }
-}
+        }, 1);
+        e.preventDefault();
+  } else {
+            console.log('Hello!!!')
+         clearInterval(interval);
 
-var createHtml = function(){
+            e.preventDefault();
 
- ms.innerHTML = deltaTime.getMilliseconds();
- seconds.innerHTML = deltaTime.getSeconds();
- minutes.innerHTML = deltaTime.getMinutes();
- hours.innerHTML = deltaTime.getUTCHours();
- isActive = true;
+        }
+    }
 
-};
+    var createHtml = function(){
 
-startBtn.addEventListener('click', getTime, false);
+        ms.innerHTML = deltaTime.getMilliseconds();
+        seconds.innerHTML = deltaTime.getSeconds();
+        minutes.innerHTML = deltaTime.getMinutes();
+        hours.innerHTML = deltaTime.getUTCHours();
+        isActive = true;
+    };
 
+    startBtn.addEventListener('click', getTime);
+    // stopBtn.addEventListener('click', stopTimer);
 }());
